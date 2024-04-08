8d34181e6bc8385d1fedd698bb9810840c376a90
Amanda <amanda_feijoo@hotmail.com>
2024-04-08 21:14:02 +0200

    updates

--- src/test/setupTest.js
+++
@@ -1,6 +0,0 @@
-
-import { setupServer } from 'msw/node';
-import { handlers } from './mocks/handlers';
-const server = setupServer(...handlers);
-beforeAll(() => server.listen());
-afterAll(() => server.resetHandlers(), server.close());
